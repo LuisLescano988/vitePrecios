0345e5d598bee99ec9918a099146e5b41956cb49
Luis Lescano <luislescanologo@hotmail.com>
2023-12-21 18:54:36 -0300

    .

--- src/views/DeleteProduct.jsx
+++ src/views/DeleteProduct.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux';
-import { deleteProduct, getAllProducts, updateProduct } from '../redux/actions';
+import { deleteProduct, getAllProducts } from '../redux/actions';
 import { useNavigate } from 'react-router-dom'
 
 const DeleteProduct = () => {
@@ -10,7 +10,6 @@ const DeleteProduct = () => {
 
     const [search, setSearch] = useState('');
     const [selectedProduct, setSelectedProduct] = useState(null);
-    const [confirmation, setConfirmation] = useState(false);
 
     const getFilteredProducts = (search, allProducts) => {
         if (!search) {
@@ -31,83 +30,75 @@ const DeleteProduct = () => {
 
     const handleRowClick = (product) => {
         setSelectedProduct(product);
-        // setConfirmation(true);
-    };
-    
-    const handleConfirmedUpdate = () => {
-        try {
-            dispatch(deleteProduct(selectedProduct));
-            setConfirmation(true);
-        } catch (error) {
-            console.error('Error al actualizar el producto', error);
-        }
     };
 
-    const handleCancelUpdate = () => {
-        setConfirmation(false);
+    const handleConfirmedDelete = () => {
+        setSelectedProduct(null);
+        setSearch('');
+    };
+
+    const handleCancelDelete = () => {
         setSelectedProduct(null);
     };
 
 
     return (
         <div className=' bg-black h-screen'>
-            {!confirmation ?
-                <div>
-                    <div className=" pt-8">
-                        <p className=' text-5xl'>Busque el producto que desea actualizar.</p>
-                        <input type="text"
-                            className=' text-black w-4/5 text-center italic rounded-sm mt-3'
-                            placeholder='Ingrese un código, nombre de producto o proveedor para su busqueda.'
-                            onChange={(event) => setSearch(event.target.value)} />
-                    </div>
-                    {search && (
-                        <div>
-                            <table className=" text-left mt-8 w-full">
-                                <thead className="">
-                                    <tr>
-                                        <th colSpan="1">Nombre</th>
-                                        <th colSpan="1">Codigo</th>
-                                        <th colSpan="1">Precio de compra</th>
-                                        <th colSpan="1">Precio de venta</th>
-                                        <th colSpan="1">Margen de venta</th>
-                                        <th colSpan="1">Venta por</th>
+            <div>
+                <div className=" pt-8">
+                    <p className=' text-5xl'>Busque el producto que desea eliminar.</p>
+                    <input type="text"
+                        className=' text-black w-4/5 text-center italic rounded-sm mt-3'
+                        value={search}
+                        placeholder='Ingrese un código, nombre de producto o proveedor para su busqueda.'
+                        onChange={(event) => setSearch(event.target.value)} />
+                </div>
+                {search && (
+                    <div>
+                        <table className=" text-left mt-8 w-full">
+                            <thead className="">
+                                <tr>
+                                    <th colSpan="1">Nombre</th>
+                                    <th colSpan="1">Codigo</th>
+                                    <th colSpan="1">Precio de compra</th>
+                                    <th colSpan="1">Precio de venta</th>
+                                    <th colSpan="1">Margen de venta</th>
+                                    <th colSpan="1">Venta por</th>
+                                </tr>
+                            </thead>
+                            <tbody className="">
+                                {filteredProducts.map((product) => (
+                                    <tr className=" text-sm  border-b-2 hover:bg-green-800"
+                                        key={product.id}
+                                        onClick={() => { handleRowClick(product) }}>
+                                        <td className="font-bold ">{product.product_name}</td>
+                                        <td className="">{product.supplier_code}</td>
+                                        <td className="">{product.purchase_price}</td>
+                                        <td className="">{product.sales_price}</td>
+                                        <td className="">{product.percent}%</td>
+                                        <td className="">{product.messure_unit}</td>
                                     </tr>
-                                </thead>
-                                <tbody className="">
-                                    {filteredProducts.map((product) => (
-                                        <tr className=" text-sm  border-b-2 hover:bg-green-800"
-                                            key={product.id}
-                                            onClick={() => { handleRowClick(product) }}>
-                                            <td className="font-bold ">{product.product_name}</td>
-                                            <td className="">{product.supplier_code}</td>
-                                            <td className="">{product.purchase_price}</td>
-                                            <td className="">{product.sales_price}</td>
-                                            <td className="">{product.percent}%</td>
-                                            <td className="">{product.messure_unit}</td>
-                                        </tr>
-                                    ))}
-                                </tbody>
-                            </table>
+                                ))}
+                            </tbody>
+                        </table>
+                    </div>
+                )}
+                {selectedProduct && (
+                    <div className="fixed inset-0 bg-gray-800 bg-opacity-75 flex items-center justify-center">
+                        <div className="bg-white p-4 rounded-md">
+                            <p className="text-xl mb-4 text-black">¿Realmente desea borrar este producto?</p>
+                            <button className="bg-green-500 text-white py-2 px-4 rounded mr-2"
+                                onClick={handleConfirmedDelete}>
+                                Sí, eliminar
+                            </button>
+                            <button className="bg-red-500 text-white py-2 px-4 rounded"
+                                onClick={handleCancelDelete}>
+                                Cancelar
+                            </button>
                         </div>
-                    )}
-                    {selectedProduct && (
-                        <div className="fixed inset-0 bg-gray-800 bg-opacity-75 flex items-center justify-center">
-                            <div className="bg-white p-4 rounded-md">
-                                <p className="text-xl mb-4 text-black">¿Realmente desea borrar este producto?</p>
-                                <button className="bg-green-500 text-white py-2 px-4 rounded mr-2"
-                                    onClick={handleConfirmedUpdate}>
-                                    Sí, actualizar
-                                </button>
-                                <button className="bg-red-500 text-white py-2 px-4 rounded"
-                                    onClick={handleCancelUpdate}>
-                                    Cancelar
-                                </button>
-                            </div>
-                        </div>
-                    )}
-                </div> :
-                <div>Manuelita vivia en Pehuajo, pero un dia se marcho</div>
-            }
+                    </div>
+                )}
+            </div>
         </div>
     )
 }
